b061ed616f68302091a91f1af0223c85ac29b872
Tom Akers <akers.thomas@yahoo.co.uk>
2022-09-07 17:13:30 +0100

    new dark mode button and code clean-up

--- java-index.js
+++
@@ -1,9 +0,0 @@
-function darkMODE(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
-
-function darkMODEcard(){
-    var element = document.body;
-    element.classList.toggle("card-base-dark");
-}
